c5df994ac37a0fc274c7d4fe4d753a93de983010
Gwénaël Gabard <gwenael.gabard@univ-lemans.fr>
2022-09-24 08:57:10 +0200

    Add constraints to the linear system

--- pfe/algebra/linear_system.py
+++ pfe/algebra/linear_system.py
@@ -1,7 +1,8 @@
 """Linear system with sparse matrices"""
 import numpy as np
-from scipy.sparse import coo_matrix
+from scipy.sparse import coo_matrix, bmat
 from scipy.sparse.linalg import spsolve
+from numpy.linalg import qr, solve
 
 
 class Terms:
@@ -81,6 +82,24 @@ class Terms:
         self.v = [M.data.copy()]
 
 
+class Constraint:
+    """A linear constraint to impose on the linear system
+    """
+    def __init__(self, dof, coef, rhs):
+        """Default constructor
+
+        :param dof: list of dof numbers
+        :type dof: list or numpy array
+        :param coef: list of coefficients
+        :type coef: list or numpy array
+        :param rhs: rhs value of the constraint, defaults to 0.0
+        :type rhs: float or complex, optional
+        """
+        self.dof = dof
+        self.coef = coef
+        self.rhs = rhs
+
+
 class LinearSystem:
     r"""Linear system based on a sparse matrix assembled from element contributions.
 
@@ -101,6 +120,7 @@ class LinearSystem:
         self.lhs = Terms()
         self.rhs = Terms()
         self.num_dofs = 0
+        self.constraints = []
 
     def clear(self):
         """Clear the right-hand-side and left-hand-side matrices as well as the number of
@@ -109,6 +129,7 @@ class LinearSystem:
         self.lhs.clear()
         self.rhs.clear()
         self.num_dofs = 0
+        self.constraints = []
 
     def get_dofs(self, num):
         """Declare additional degrees of freedom in the linear system
@@ -123,6 +144,18 @@ class LinearSystem:
         self.num_dofs += total
         return dofs
 
+    def add_constraint(self, dof, coef, rhs=0.0):
+        """Add a linear constraint
+
+        :param dof: list of dof numbers
+        :type dof: list or numpy array
+        :param coef: list of coefficients
+        :type coef: list or numpy array
+        :param rhs: rhs value of the constraint, defaults to 0.0
+        :type rhs: float or complex, optional
+        """
+        self.constraints.append(Constraint(dof,coef,rhs))
+
     def assemble(self):
         """Assemble the left- and right-hand sides of the system"""
         self.lhs.assemble()
@@ -134,6 +167,10 @@ class LinearSystem:
         :return: The solution vector
         :rtype: Numpy array (potentially complex valued)
         """
+        # Check if constraints are defined
+        if self.constraints:
+            return self.csolve()
+        # Solve without constraint
         A = self.lhs.matrix().tocsc()
         A.resize((self.num_dofs, self.num_dofs))
         b = self.rhs.matrix()
@@ -141,3 +178,55 @@ class LinearSystem:
         b = b.todense()
         x = spsolve(A, b)
         return x
+
+    def csolve(self):
+        """Solve the sparse linear system together with linear constraints
+
+        :return: The solution vector
+        :rtype: Numpy array (potentially complex valued)
+        """
+        # Number of constraints
+        m = len(self.constraints)
+        # Create a list of dof involved in the constraints
+        nc = []
+        for c in self.constraints:
+            nc += list(c.dof)
+        nc = np.unique(nc).astype(int)
+        p = len(nc)
+        # Create a list of dof not involved in the constraints
+        nf = np.arange(self.num_dofs)
+        nf[nc] = -1
+        nf = np.unique(nf)[1:]
+        # Create the matrix of constraints
+        t = Terms()
+        d = []
+        for n, c in enumerate(self.constraints):
+            t.add([n]*len(c.dof), c.dof, c.coef)
+            d.append(c.rhs)
+        C = t.matrix().tocsr()[:,nc].toarray()
+        d = np.array(d)
+        # QR factorization of C
+        Q, R = qr(C.T, 'complete')
+        Q2 = Q[:,m:]
+        # Solve for the constrained subspace
+        x1 = Q[:,:m]@solve(R[:m,:].T, d)
+        # Assemble the lhs and rhs of the system
+        A = self.lhs.matrix().tocsr()
+        A.resize((self.num_dofs, self.num_dofs))
+        b = self.rhs.matrix()
+        b.resize((self.num_dofs, 1))
+        b = b.toarray().flatten()
+        # Build the reduced linear system (without the constrained subspace)
+        Afc = A[np.ix_(nf, nc)]
+        Acc = A[np.ix_(nc, nc)]
+        A2 = bmat([[Q2.T@Acc@Q2, Q2.T@A[np.ix_(nc, nf)]], [Afc@Q2, A[np.ix_(nf, nf)]]]).tocsc()
+        b2 = Q2.T@(b[nc]-Acc@x1)
+        bf = b[nf] - Afc@x1
+        bc = np.vstack((b2[:,None],bf[:,None]))
+        # Solve the reduced system
+        x = spsolve(A2, bc)
+        # Build the full solution vector
+        xx = np.zeros((self.num_dofs,), dtype=complex)
+        xx[nc] = x1 + Q2@x[:len(b2)]
+        xx[nf] = x[len(b2):]
+        return xx
