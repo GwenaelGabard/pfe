1a763cce6a26216835796aa0229bf60448c9fe4f
Gwénaël Gabard <gwenael.gabard@univ-lemans.fr>
2025-11-29 14:10:21 +0100

    Add models in setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,9 @@ from setuptools import setup, Command
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 class CreateTables(Command):
-    description = 'Generate look-up tables'
+    description = "Generate look-up tables"
 
     user_options = []
 
@@ -16,29 +17,44 @@ class CreateTables(Command):
 
     def finalize_options(self):
         pass
-            
+
     def run(self):
         print("Generating quadrature rules...")
         subprocess.call("python make_quadrature.py", shell=True, cwd="pfe/quadrature")
         print("Generating look-up tables for basis functions...")
         subprocess.call("python make_basis.py", shell=True, cwd="pfe/shape")
 
+
 setup(
-    name = "pfe",
-    version = "0.1.0",
-    author = "Gwénaël Gabard",
-    author_email = "gwenael.gabard@univ-lemans.fr",
-    description = ("Python Finite Elements"),
-    license = "MIT",
-    keywords = "finite element, numerical simulation, computational modelling",
-    url = "https://github.com/GwenaelGabard/pfe",
-    packages=['pfe', 'pfe.geometry', 'pfe.shape', 'pfe.quadrature', 'pfe.algebra', 'pfe.interpolation', 'pfe.models'],
-    long_description=read('README.md'),
+    name="pfe",
+    version="0.1.0",
+    author="Gwénaël Gabard",
+    author_email="gwenael.gabard@univ-lemans.fr",
+    description=("Python Finite Elements"),
+    license="MIT",
+    keywords="finite element, numerical simulation, computational modelling",
+    url="https://github.com/GwenaelGabard/pfe",
+    packages=[
+        "pfe",
+        "pfe.geometry",
+        "pfe.shape",
+        "pfe.quadrature",
+        "pfe.algebra",
+        "pfe.interpolation",
+        "pfe.models",
+        "pfe.models.helmholtz_2d",
+        "pfe.models.lpe_2d",
+        "pfe.models.lpe_axi",
+    ],
+    long_description=read("README.md"),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    cmdclass={'create_tables': CreateTables,},
+    cmdclass={
+        "create_tables": CreateTables,
+    },
+    package_data={"pfe": ["shape/*.npy", "quadrature/*.npy"]},
 )
